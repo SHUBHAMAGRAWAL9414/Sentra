30c0f578120dea05d868039fcfc750eadbb9c7c5
SHUBHAMAGRAWAL9414 <140111040+SHUBHAMAGRAWAL9414@users.noreply.github.com>
2025-07-13 10:43:41 +0530

    Update SignUp.jsx

--- Frontend/src/pages/SignUp.jsx
+++ Frontend/src/pages/SignUp.jsx
@@ -5,7 +5,7 @@ import { IoEyeOff } from "react-icons/io5";
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
-import { userDataContext } from '../context/userContext';
+import { userDataContext } from '../context/UserContext';
 import axios from "axios";
 
 const SignUp = () => {
